dac44a88760da2a4f22ef397d976ee7f04ffb9a4
pravinkuyate <pravinkuyate0@gmail.com>
2024-01-03 15:27:08 +0530

    2nd commit by pravin

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #grading system main code
+mport pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings("ignore")
+from matplotlib.ticker import MaxNLocator
+import os
+file_path=r"C:\Users\pravinkuyate\Downloads\Copy of Technical Training feedback of trainer.xlsx"
+df=pd.read_excel(file_path)
+df.dropna(inplace=True)
